2cb482cc36d4900e77169ee873fb36097a39fe89
Jacek <jacekwilczynski5@gmail.com>
2018-09-19 10:32:15 +0200

    Make links scroll page to different sections (#45)
    * Put ids on sections
    
    * Point navbar links to sections
    
    * Make buttons into anchors with hrefs
    
    * Give anchors made from buttons the same position
    
    * Point "View details" link to "ready" section
    
    * Collect inner links
    
    * Add smooth scrolling behavior

---
+++ scripts/smooth-scroll.js
@@ -0,0 +1,22 @@
+(function() {
+  function getInnerLinks() {
+    var links = document.getElementsByTagName("a");
+    var innerLinks = [];
+    for (var i = 0; i < links.length; i++) {
+      var href = links[i].getAttribute("href");
+      if (href && href[0] === "#") innerLinks.push(links[i]);
+    }
+    return innerLinks;
+  }
+
+  var innerLinks = getInnerLinks();
+  innerLinks.forEach(function(link) {
+    var href = link.getAttribute("href");
+    var targetId = href.slice(1);
+    var target = document.getElementById(targetId);
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      window.scrollTo({ top: target.offsetTop, behavior: "smooth" });
+    });
+  });
+})();
